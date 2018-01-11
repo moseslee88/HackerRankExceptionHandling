1d8f2e8b900aba88060bfb70e36cb72a3d8faa09
Moses <moseslee88@gmail.com>
2018-01-11 17:50:54 +0000

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -5,8 +5,25 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
-    public static void main(String[] args) {        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */                //Java has built-in mechanism to handle exceptions. Using the try statement we can test a         //block of code for errors. The catch block contains the code that says what to do if exception occurs. 
-//This problem will test your knowledge on try-catch block.
+    public static void main(String[] args) {      
+      /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */    
+      
+      //Java has built-in mechanism to handle exceptions. Using the try statement we can test a    
+      //block of code for errors. The catch block contains the code that says what to do if exception occurs. 
+
 //You will be given two integers  and  as input, you have to compute . If  and  are not  bit signed integers or if  is zero, exception will occur and you have to report it. Read sample Input/Output to know what to report in case of exceptions.
-           try (Scanner in = new Scanner(System.in)) {     int x = in.nextInt();     int y = in.nextInt();     System.out.println(x / y);                     in.close();   }        catch (Throwable t) {     System.out.println(t.getClass().getName()       + ((t instanceof ArithmeticException) ? (": " + t         .getMessage()) : ""));    } 
-               }}
+          
+      
+      try (Scanner in = new Scanner(System.in)) { 
+        int x = in.nextInt();
+        int y = in.nextInt(); 
+        System.out.println(x / y);
+        in.close();  
+      }      
+      catch (Throwable t) {
+        System.out.println(t.getClass().getName() + ((t instanceof ArithmeticException) ? (": " + t.getMessage()) : ""));
+      } 
+               }
+
+
+}
